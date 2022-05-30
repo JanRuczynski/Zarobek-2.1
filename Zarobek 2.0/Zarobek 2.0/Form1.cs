1d87e3f8cb905fce8fec8804f94fbefcf5c2bf57
Żan <konto564@wp.pl>
2022-05-30 20:31:10 +0200

    Add files via upload

---
+++ Zarobek 2.0/Zarobek 2.0/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zarobek_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int godziny = int.Parse(textBox1.Text);
+            int zamowienia = int.Parse(textBox2.Text);
+            double minuty = int.Parse(textBox4.Text);
+
+            double godzinyd = godziny + (minuty / 60);
+
+            double zarobek = godzinyd * 9.9 + zamowienia * 5;
+            double zarobekh = zarobek / godzinyd;
+            
+            if(zarobekh < 19.9)
+            {
+                zarobekh = 19.9;
+            }
+
+
+
+            label4.Text = "Dzisiejszy zarobek to " + zarobek;
+            label5.Text = "Zarobek na godzinę to " + zarobekh;
+        }
+    }
+}
